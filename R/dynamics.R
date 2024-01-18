fafcab9c7b1e852c6e033bf4f2552a81536ffb5f
David Smith <smitdave@gmail.com>
2024-01-18 10:58:18 -0800

    Documentation for dynamics.R

--- R/dynamics.R
+++ R/dynamics.R
@@ -1,13 +1,13 @@
 
-#' Title
+#' Compute the density of parasite infections in a host cohort
 #' @description
-#'
+#' Solutions to the time and age version of the Ross-Macdonald model.
 #' @param alpha a parasites age of infection
 #' @param a the host cohort age
-#' @param FoIpar
-#' @param hhat
-#' @param tau
-#' @param r
+#' @param FoIpar parameters that define an FoI function
+#' @param hhat a local scaling parameter for the FoI
+#' @param tau the cohort birthday
+#' @param r the clearance rate for a simple infection
 #'
 #' @return
 #' @export
