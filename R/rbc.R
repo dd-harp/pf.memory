e1d1d293ff09f83ea6be3f3cbf67775e9121928b
David Smith <smitdave@gmail.com>
2024-01-18 11:05:26 -0800

    Documentation for rbc.R

--- R/rbc.R
+++ R/rbc.R
@@ -1,9 +1,36 @@
+#' Compute log10 of the red blood cell population
+#'
+#' @param a the age of a host cohort
+#' @param par a [list] of parameters
+#'
+#' @return a [numeric] vector of length(a)
+#' @export
+#'
+#' @examples
 log10RBC = function(a,par){
   UseMethod("log10RBC", par)
 }
 
-log10RBC.0 = function(a,par){par$lRBCmax }
+#' Compute log10 of the red blood cell population
+#' @description
+#' A static model
+#'
+#' @inheritParams log10RBC
+#'
+#' @return a [numeric] vector of length(a)
+#' @export
+#'
+#' @examples
+log10RBC.0 = function(a,par){par$lRBCmax}
 
+#' Set up parameters for [log10RBC.0]
+#'
+#' @param lRBCmax the maximum
+#'
+#' @return a [list]
+#' @export
+#'
+#' @examples
 par_lRBC.0 = function(lRBCmax=13){
   par = list()
   class(par) <- "0"
