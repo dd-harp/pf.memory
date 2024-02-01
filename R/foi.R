cfe1a1fbb1c84be28538b4d448b79882c8c9a1da
David Smith <smitdave@gmail.com>
2024-02-01 09:32:50 -0800

    updated vignette

--- R/foi.R
+++ R/foi.R
@@ -14,3 +14,31 @@ FoI = function(a, par, tau=0, hhat=NULL){with(par,{
     seasonalFoI(a+tau,seasonPar)*
     trendFoI(a+tau,trendPar)
 })}
+
+make_FoI = function(par){
+  par1 = par
+  par1$agePar = par_flatAge()
+  norm1 = integrate(FoI, 0, 365, par=par1)$value
+  par2 = par
+  par2$seasonalFoI = par_flatSeason()
+  par2$trendFoI = par_flatTrend()
+  norm2 = integrate(FoI, 0, 365, par=par2)$value
+  par$hbar = 1/norm1/norm2
+  ff = function(a, tau=0){
+    FoI(a, par, tau)
+  }
+  return(ff)
+}
+
+make_FoI = function(par){
+  norm = integrate(FoI, 0, 55*365, par=par1)$value
+  par$hbar = 1/norm/55
+  ff = function(a, tau=0){
+    FoI(a, par, tau)
+  }
+  return(ff)
+}
+
+
+f1 = make_FoI(foiP3)
+integrate(f1, 0, 365)$value
