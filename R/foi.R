04ef302f52fbffd3aed853d685b1b99f3a4280f2
David Smith <smitdave@gmail.com>
2024-02-02 10:17:24 -0800

    prevalence vignette added

--- R/foi.R
+++ R/foi.R
@@ -15,30 +15,18 @@ FoI = function(a, par, tau=0, hhat=NULL){with(par,{
     trendFoI(a+tau,trendPar)
 })}
 
-make_FoI = function(par){
-  par1 = par
-  par1$agePar = par_flatAge()
-  norm1 = integrate(FoI, 0, 365, par=par1)$value
-  par2 = par
-  par2$seasonalFoI = par_flatSeason()
-  par2$trendFoI = par_flatTrend()
-  norm2 = integrate(FoI, 0, 365, par=par2)$value
-  par$hbar = 1/norm1/norm2
+#' Trace Function - Force of Infection
+#'
+#' @param par a formatted list
+#' @param maxAge age in years to normalize over
+#'
+#' @return [numeric]
+#' @export
+make_FoI = function(par, maxAge = 50){
+  norm = integrate(FoI, 0, maxAge*365, tau=0, par=par)$value
+  par$hbar = maxAge*par$hbar/norm
   ff = function(a, tau=0){
     FoI(a, par, tau)
   }
   return(ff)
 }
-
-make_FoI = function(par){
-  norm = integrate(FoI, 0, 55*365, par=par1)$value
-  par$hbar = 1/norm/55
-  ff = function(a, tau=0){
-    FoI(a, par, tau)
-  }
-  return(ff)
-}
-
-
-f1 = make_FoI(foiP3)
-integrate(f1, 0, 365)$value
